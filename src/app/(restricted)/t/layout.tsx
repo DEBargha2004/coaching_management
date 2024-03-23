2f9a9bad545b3d9259b05af3f4a1f39f50a64b75
DEBargha2004 <debarghasaha16@gmail.com>
2024-03-23 20:20:57 +0530

    feat - breadcrumbs added

--- src/app/(restricted)/t/layout.tsx
+++ src/app/(restricted)/t/layout.tsx
@@ -1,7 +1,59 @@
-export default async function Layout ({
-  children
-}: {
-  children: React.ReactNode
-}) {
-  return <section className='w-full h-full'>{children}</section>
+'use client'
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator
+} from '@/components/ui/breadcrumb'
+import { usePathname } from 'next/navigation'
+import { useMemo } from 'react'
+
+export default function Layout ({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  let pathnameArray = useMemo(() => {
+    let pathArray = pathname?.split('/')
+    pathArray.shift()
+    pathArray = pathArray.join('/separator/').split('/')
+    return pathArray
+  }, [pathname])
+
+  return (
+    <section className='w-full h-full'>
+      <div className='h-[5%]'>
+        <Breadcrumb>
+          <BreadcrumbList>
+            {pathnameArray.map((path, path_idx) => {
+              return (
+                <>
+                  {path === 'separator' ? (
+                    <BreadcrumbSeparator />
+                  ) : (
+                    <BreadcrumbItem
+                      className={
+                        path === pathnameArray.at(-1)
+                          ? 'text-primary-foreground'
+                          : ''
+                      }
+                    >
+                      <BreadcrumbLink
+                        href={`/${pathnameArray
+                          .slice(0, path_idx + 1)
+                          .join('/')
+                          .replaceAll('/separator', '')}`}
+                      >
+                        {path}
+                      </BreadcrumbLink>
+                    </BreadcrumbItem>
+                  )}
+                </>
+              )
+            })}
+          </BreadcrumbList>
+        </Breadcrumb>
+      </div>
+      <div className='h-[95%]'>{children}</div>
+    </section>
+  )
 }
