73de8dc5252e9192395d80cca6a98fbd157ebbd5
DEBargha2004 <debarghasaha16@gmail.com>
2024-04-15 20:08:09 +0530

    feat
    jiti added for validation of env at build time rather than run time

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createJITI from 'jiti'
+import { fileURLToPath } from 'node:url'
 
-export default nextConfig;
+const jiti = createJITI(fileURLToPath(import.meta.url))
+
+jiti('./src/schema/env/server.ts')
+jiti('./src/schema/env/client.ts')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default nextConfig
